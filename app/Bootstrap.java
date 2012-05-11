fef818868e4be3c0d953bdc865f036813d263302
Corey Brady <cbrady@inquirelearning.com>
2012-05-11 00:03:32 -0500

    alternative defaults

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -11,7 +11,22 @@ public class Bootstrap extends Job {
         //Check if the database is empty
         if(School.count() == 0) {
         	Fixtures.deleteDatabase();
-            Fixtures.loadModels("seeddata.yml");
+            //Fixtures.loadModels("seeddata.yml");
+            School s = new School("SST");
+            s.save();
+            Teacher t = new Teacher("Johari", s);
+            t.save();
+            String ubase = "S";
+            Classroom c = new Classroom(s, t, "Test Class", 2012);
+            c.save();
+            for (int i = 1; i<31; i++ )
+            {
+            	String uid = String.valueOf(i);
+            	if (i<10)
+            		uid = "0"+uid;
+            	StudentUser su = new StudentUser(ubase+uid, c);
+            	su.save();
+            }
         }
     }
  
