88157d59857aae778e5f21b30878f2952f18b709
Corey Brady <cbrady@inquirelearning.com>
2013-02-05 09:50:48 -0600

    correct seed data

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -10,64 +10,66 @@ import models.*;
 @OnApplicationStart
 public class Bootstrap extends Job {
  
-    @SuppressWarnings("unused")
 	public void doJob() {
         //Check if the database is empty
         if(School.count() == 0) {
         	System.err.println("REMOVING ALL DATABASE ENTRIES....");
         	Fixtures.deleteDatabase();
             //Fixtures.loadModels("seeddata.yml");
-            School s = new School("SST");
-            s.save();
-            School s2 = new School("ANOTHER");
-            s2.save();
-            Teacher t = new Teacher("Johari", s);
-            t.save();
-            Teacher t2 = new Teacher("Another", s);
-            t2.save();
-            String ubase = "S";
-            Classroom c = new Classroom(s, t, "Test Class", 2012);
-            c.save();
-            Classroom c2 = new Classroom(s, t, "A Second Class", 2012);
-            c2.save();
-            ArrayList<StudentUser>sus = new ArrayList<StudentUser>();
-            for (int i = 1; i<31; i++ )
-            {
-            	String uid = String.valueOf(i);
-            	if (i<10)
-            		uid = "0"+uid;
-            	StudentUser su = new StudentUser(ubase+uid, c);
-            	su.save();
-            	sus.add(su);
-            }
+        	School s = new School("Test");
+        	s.save();
+
+
+        	//School s2 = new School("ANOTHER");
+        	//s2.save();
+        	//Teacher t = new Teacher("Corey", s);
+        	//t.save();
+        	Teacher t = new Teacher("DataTester", s);
+        	t.save();
+        	String ubase = "S";
+        	Classroom c = new Classroom(s, t, "Test Class", 2012);
+        	c.save();
+        	//Classroom c2 = new Classroom(s, t, "A Second Class", 2012);
+        	//c2.save();
+        	ArrayList<StudentUser>sus = new ArrayList<StudentUser>();
+        	for (int i = 1; i<31; i++ )
+        	{
+        		String uid = String.valueOf(i);
+        		if (i<10)
+        			uid = "0"+uid;
+        		StudentUser su = new StudentUser(ubase+uid, c);
+        		su.save();
+        		sus.add(su);
+        	}
+
+        	//create a fake session
+        	//Application.startActivity(t.username, t.school.schoolName, c.classname, c.startYear, "Test Activity", "Dummy Loader");
+        	//Application.startActivity("Johari", "SST", "Test Class", 2012, "Test Activity", "Dummy Loader");
+        	Session se = new Session(c, "Fake Source");
+        	se.save();
+
+        	StudentUser csu = new StudentUser("autogenstudent", c);
+        	csu.save();
+
+        	for (int i=1; i<100; i++)
+        	{
+        		double co = i / 10.0;
+        		String fn = String.valueOf(co) + "sin(X)";
+        		Contribution con;
+        		if ( i % 30 == 0 )
+        		{
+        			con = new Contribution(ContributionType.EQUATION, csu, se, "Y"+i, "h" + fn + "g", false );
+        		}
+        		else
+        		{
+        			con = new Contribution(ContributionType.EQUATION, sus.get(i%30), se, "Y"+i, fn, true );
+        		}
+        		Long tim = con.timestamp.getTime();
+        		con.timestamp.setTime( tim + i * 1000);
+        		con.secondsIn += i;
+        		con.save();
+        	}
             
-            //create a fake session
-            //Application.startActivity(t.username, t.school.schoolName, c.classname, c.startYear, "Test Activity", "Dummy Loader");
-            //Application.startActivity("Johari", "SST", "Test Class", 2012, "Test Activity", "Dummy Loader");
-            Session se = new Session(c, "Fake Source");
-            se.save();
-            
-            StudentUser csu = new StudentUser("corey", c);
-            csu.save();
-            
-            for (int i=1; i<100; i++)
-            {
-            	double co = i / 10.0;
-            	String fn = String.valueOf(co) + "sin(X)";
-            	Contribution con;
-            	if ( i % 30 == 0 )
-            	{
-            		con = new Contribution(ContributionType.EQUATION, csu, se, "Y"+i, "h" + fn + "g", false );
-            	}
-            	else
-            	{
-            		 con = new Contribution(ContributionType.EQUATION, sus.get(i%30), se, "Y"+i, fn, true );
-            	}
-            	Long tim = con.timestamp.getTime();
-       		 	con.timestamp.setTime( tim + i * 1000);
-       		 	con.secondsIn += i;
-	    		con.save();
-            }
             
             CodeCategory cca = new CodeCategory("Math");
             CodeCategory ccb = new CodeCategory("Social");
