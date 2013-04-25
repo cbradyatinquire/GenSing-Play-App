8e98f63888b7085411dd9414da0e4ffe5c4422d9
David Effendi <deffendi@gmail.com>
2013-04-25 01:26:47 +0000

    added getCodeCategories and getCodeDescriptorsByCategory ( buggy )

--- app/models/CodeDescriptor.java
+++ app/models/CodeDescriptor.java
@@ -1,10 +1,13 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
 import play.data.validation.Required;
 import play.db.jpa.Model;
+
 
 @Entity
 public class CodeDescriptor extends Model {
@@ -22,6 +25,12 @@ public class CodeDescriptor extends Model {
 		this.descri = d;
 		this.ccategory = ca;
 	}
+
+        public static List<CodeDescriptor> findByCategory( CodeCategory cc ) {
+            String qu = "select c from CodeDescriptor c WHERE c.ccategory = " + cc;
+            List<CodeDescriptor> cds = CodeDescriptor.find(qu, this).fetch();
+            return cds;
+        }
 
 	public static CodeDescriptor findByCategoryAndName(CodeCategory cc,
 			String descri) {
